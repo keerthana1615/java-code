abf3dd0a39c6e2f4af61ab4424f85ab1e94af5f3
keerthana1615 <keerthanasivasamy7@gmail.com>
2025-12-12 12:21:58 +0530

    Update Q23.java

--- Q23.java
+++ Q23.java
@@ -4,9 +4,6 @@ public class Q23
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		String a =s.nextLine();
-		String b =s.nextLine();
-		String c =s.nextLine();
-		String d =s.nextLine();
-		System.out.println(a+"\n"+b+"\n"+c+"\n"+d);
+		System.out.println("your blood group is:"+a);
 	}
 }
